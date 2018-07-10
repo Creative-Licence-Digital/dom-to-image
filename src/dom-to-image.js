42d0e495fb0fa206d49a07607fb1104a4ea94df3
Stephen Hutchings <stephen@creativelicence.com.au>
2018-07-10 15:43:21 +1000

    Fix font rendering tsayen#225

--- src/dom-to-image.js
+++ src/dom-to-image.js
@@ -216,6 +216,8 @@
                     if (source.cssText) target.cssText = source.cssText;
                     else copyProperties(source, target);
 
+                    target.fontStretch = 'normal';
+
                     function copyProperties(source, target) {
                         util.asArray(source).forEach(function (name) {
                             target.setProperty(
